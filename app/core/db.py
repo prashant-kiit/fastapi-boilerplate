2a1b34a98b4082cd6c297ec47b500c77595b16f6
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-25 05:00:55 +0530

    Add initial implementation of CRUD operations with database session management; introduce new files for core configuration and CRUD logic

--- app/core/db.py
+++ app/core/db.py
@@ -1,10 +1,18 @@
+from fastapi import Request
 from sqlmodel import Session, SQLModel, create_engine
 
 engine = create_engine("sqlite:///./todo.db")
 
+
 def init_db():
     SQLModel.metadata.create_all(engine)
 
-def get_session():
+
+def get_session(request: Request):
     with Session(engine) as session:
-        yield session
+        try:
+            # request.state['db_session'] = session
+            yield session
+        except Exception:
+            session.rollback()
+            raise
